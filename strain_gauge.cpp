6863073ec7f65823ba7d654d498f50ca41024d2a
Ethan-Cleveland <thnclvlnd@tamu.edu>
2025-11-10 18:35:02 -0600

    Add files via upload
    Calibrated values, mostly

--- strain_gauge.cpp
+++ strain_gauge.cpp
@@ -1,6 +1,6 @@
 //ETHAN CLEVELAND | TAMU DBF | 9/17/2025
 //150.5 
-//THIS CODE IS CREATED TO TAKE IN data FROM THE STRAIN GUAGE, CONVERT THEM TO FORCE IN NEWTONS
+//THIS CODE IS CREATED TO TAKE IN data FROM THE STRAIN GUAGE, CONVERT THEM TO FORCE IN GRAMS
 //AND UPLOAD TO A CSV ON AN SD CARD
 //UPLOAD TIME IN FIRST COLLUMN AND FORCE IN SECOND
 //NOTE: WILL NEED TO EXPERIMENT TO FIND FORCE CONVERSION
@@ -14,9 +14,9 @@
 
 
 // Variables
-const int DT_pin = 19; //The pin on the arduino that is connected to the DT pin on the amplifier
-const int SCK_pin = 18; //The pin on the arduino that is connected to the SCK pin on the amplifier
-const int SD_pin = 10; //the pin the sd card is connected to
+const int DT_pin = 0; //The pin on the arduino that is connected to the DT pin on the amplifier
+const int SCK_pin = 1; //The pin on the arduino that is connected to the SCK pin on the amplifier
+const int SD_pin = BUILTIN_SDCARD; //the pin the sd card is connected to
 
 //THESE ARE CHEAP PRESSURE COMMENT OUT IF NOT USING
 /*
@@ -46,30 +46,36 @@ void setup() {
   Serial.begin(9600); 
   while(!Serial); // allow port to connect
 
-/*
+
   if(!SD.begin(SD_pin)){  // dont run if sd card not in
     Serial.println("Error: No Card Detected.");
     while(1);
   }
-*/ 
-
-//I2C AIRSPEED
-  Wire.begin();
-  
-  scale.begin(DT_pin,SCK_pin); // create scale
-  scale.set_scale(conversion_factor); //  IN GRAMS
-  scale.tare(); // tare scale to set at 0
-
-  /*
+  if(SD.exists("data.csv")) SD.remove("data.csv"); //clear previous file
   data_file = SD.open("data.csv",FILE_WRITE);  // open file
-
   if(data_file){  //  if file created, set up header
-    data_file.println("Time (ms),Grams (g), Pressure (inH20), Temperature(C)"); //change after finding conversion
+    data_file.println("Time (ms),Grams (g), Pressure (PSI), Temperature(C)"); //change after finding conversion
   }
   else{
     Serial.println("Error: File cannot be opened.");
   }
- */
+
+
+//I2C AIRSPEED
+  Wire.begin();
+  Serial.println("Scanning I2C devices...");
+  Wire.beginTransmission(28);
+  if (Wire.endTransmission() == 0) {
+    Serial.print("I2C device found at address 0x");
+    Serial.println(28, HEX);
+  }
+  
+
+  scale.begin(DT_pin,SCK_pin); // create scale
+  scale.set_scale(conversion_factor); //  IN GRAMS
+  scale.tare(); // tare scale to set at 0
+
+ 
 
   start_time = millis(); // get the start time
 }
@@ -77,15 +83,20 @@ void setup() {
 void raw_data_PX4(uint16_t& raw_pressure, uint16_t& raw_temperature); //reads from wire and updates given values
 
 void loop() {  
+
+  //PRESSURE PX4
   uint16_t raw_pressure = 0;
   uint16_t raw_temperature = 0;
   
   raw_data_PX4(raw_pressure, raw_temperature); //STILL NEED TO CONVERT
-  float pressure_psi = (raw_pressure - 1638.3) / 13106.4; // conversion if correct model
+  float pressure_psi = (raw_pressure - 16383.1)*2/(16383*.8)+1.25; // conversion is in PSI, accuracy is ±0.25%
+  float temp_C = raw_temperature*200.0/2047.0-50;
 
   //  Read data from strain gauge at this time
   double data = scale.get_units(1); 
   
+
+  //TIME
   end_time = millis();
   elapsed_time = end_time - start_time; //  get timestamp
 
@@ -96,18 +107,21 @@ void loop() {
   */
 
   //write data
-  String data_string = String(elapsed_time) + "," + String(data) + "," + String(raw_pressure), String(raw_temperature);
-  //data_file.println(data_string);
-  Serial.println(data_string);
+  if(!raw_pressure == 0){
+    String data_string = String(elapsed_time) + "," + String(data) + "," + String(pressure_psi)+","+ String(temp_C);
+    data_file.println(data_string);
+    Serial.println(data_string);
+  }
+  
 
 
   //break condition
-  if(elapsed_time >= 2000000){ // change later
-    //data_file.close(); // saves to file
+  if(elapsed_time >= 10000){ // change later
+    data_file.close(); // saves to file
     Serial.println("Recording Ended.");
     while(true);
   }
-  delay(100);
+  delay(10);
 }
 
 
@@ -115,8 +129,8 @@ void raw_data_PX4(uint16_t& raw_pressure, uint16_t& raw_temperature){
   //Data Transmitts in I2C, meaning 8 bits at a time, so a 14 bit output needs two read requests
   //  Get Data From Pressure Sensor
   Wire.beginTransmission(PX4_I2C_ADDRESS);
+  Wire.endTransmission(false);
   Wire.requestFrom(PX4_I2C_ADDRESS, 4); // requests four 
-  //Wire.write(REGISTER); //probably don't need
   Wire.endTransmission(false);
   if(Wire.available() == 4){
     uint8_t pressure_MSB = Wire.read(); // Status | Pressure MSB (14-bit)
@@ -124,21 +138,25 @@ void raw_data_PX4(uint16_t& raw_pressure, uint16_t& raw_temperature){
     uint8_t temperature_MSB= Wire.read(); // Temperature MSB (11-bit)
     uint8_t temperature_LSB = Wire.read(); // Temperature LSB | CRC
 
+/*DEBUGGING*/
+    //Serial.println("Pressure "+String(pressure_MSB)+" "+String(pressure_LSB));
+
+
+
     //now we must combine the binary numbers
     //first two are status bits, get rid of them
     //bitwise AND with a mask of 00111111, AND cancels out first two, and keeps last 6 the same
-    uint16_t raw_pressure = pressure_MSB & 0x3F; //binary for 00111111
+    raw_pressure = pressure_MSB & 0x3F; //binary for 00111111
     //now shift over 8 bits, first two shifts get rid of zeroes, last 6 move up, and bitwise OR with LSB
     //that makes 14
     raw_pressure = raw_pressure << 8;
     raw_pressure |= pressure_LSB;
-  
-    //do the same for the temperature
-    uint16_t raw_temperature = temperature_MSB & 0x3F;
-    raw_temperature = raw_temperature << 8;
-    raw_temperature |= temperature_LSB;
+
+    //do the similar for the temperature
+    raw_temperature = (temperature_MSB << 3) | (temperature_LSB >> 5);
+
   }
   else{
-    Serial.println("No data values.");
+    //Serial.println("No data values.");
   }
 }
